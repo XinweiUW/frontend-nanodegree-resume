d01d3d2b1478504bcaee5a08e449addea664a546
Xinwei Liu <liuliuion@Xinweis-MacBook-Pro.local>
2016-01-04 21:44:55 -0800

    test name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name = "Xinwei Liu";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ $("#header").append(formattedName);
+
+ var role = "Web Developer";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+  $("#header").append(formattedRole);
